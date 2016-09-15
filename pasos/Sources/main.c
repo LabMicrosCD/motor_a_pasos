437a5391b38c0f805aa1d69aaa7ead7e0a95f931
Jesús Martín <a01227717@itesm.mx>
2016-09-15 15:07:54 -0500

    almost ready before changing speed equation

--- pasos/Sources/main.c
+++ pasos/Sources/main.c
@@ -3,15 +3,14 @@
 //unsigned char arreglo_medio_paso[]={0x07,0x03,0x0B,0x09,0x0D,0x0C,0x0E,0x06}; //active low
 //unsigned char arreglo_medio_paso[]={0x08,0x0C,0x04,0x06,0x02,0x03,0x01,0x09}; //active high
 //unsigned char arreglo_medio_paso[]={0x0E,0x0C,0x0D,0x09,0x0B,0x03,0x07,0x06}; //chacho
-unsigned char arreglo_medio_paso[]={0b00001000, 0b00001100, 0b00000100, 0b00000110, 0b00000010 ,0b00000011, 0b00000001, 0b00001001}; //chacho negadp
+//unsigned char arreglo_medio_paso[]={0b11110111, 0b11110011, 0b11111011, 0b11111001, 0b11111101 ,0b11111100, 0b11111110, 0b11110110};
+	unsigned char arreglo_medio_paso[]={0b00001000, 0b00001100, 0b00000100, 0b00000110, 0b00000010 ,0b00000011, 0b00000001, 0b00001001}; //chacho negadp
 unsigned char conteo = 0; 
 unsigned char i=0;
-
 unsigned char ADC_value;
-
-unsigned char range, speed;
-#define MAX_SPEED  52  // 52 miliseconds per half step to achieve minimum speed of 0.2 rpm
-#define MIN_SPEED  1   // 1 milisecond as maximum speed
+unsigned char speed, range;
+#define MAX_SPEED  4  // 52 miliseconds per half step to achieve minimum speed of 0.2 rpm
+#define MIN_SPEED  52   // 1 milisecond as maximum speed
 #define SCALE 5 	   // for each 5 values in the ADC value read with 8 bit resolution, we would have one change in our speed
 
 /* We will implement the absolut value function by using 2's complement */
@@ -22,7 +21,7 @@ unsigned char absoluteValue(unsigned char  n)
 }
 
 interrupt VectorNumber_Vadc void ADC_ISR(void){
-	range = MAX_SPEED - MIN_SPEED;
+	range = MIN_SPEED - MAX_SPEED;
 	speed = absoluteValue(ADCRL/SCALE - range);
 	ADC_value=ADCRL; //for debug purposes
 
@@ -31,19 +30,19 @@ interrupt VectorNumber_Vadc void ADC_ISR(void){
 interrupt 12 void MTIM(void){ //para la interrupcion 12, pag 66 manual PARA LOS LEDS
 	(void) MTIMSC; //se que no lo voy a leer, (TENGO QUE LEERLO CUANDO ESTE EN 1 POR PROTOCOLO DE BORRADO)
 	MTIMSC_TOF=0;// DESPUES DE LEERLO, PONERLO A 0
-	//amarillo, azul , cafe verde
-	conteo = speed;
-	do{
-		//unsigned char arreglo_medio_paso[]={0x0E,0x0C,0x0D,0x09,0x0B,0x03,0x07,0x06}; //chacho
-		__RESET_WATCHDOG();
-		//    PTBD=arreglo_medio_paso[i];
-	}while(conteo--);
-	 PTBD=arreglo_medio_paso[i];
-		    i++;
-		    if(i==8)
-		    {
-		    	i=0;
-		    }
+	//amarillo, azul-negro , cafe verde
+	if(conteo==speed){
+		 PTBD=arreglo_medio_paso[i];
+			    i++;
+			    if(i==8)
+			    {
+			    	i=0;
+			    }
+		conteo=0;
+	}else{
+		conteo++;
+	}
+
 }
 
 
