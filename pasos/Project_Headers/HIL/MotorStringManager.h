940d49d351d3d59499f2c811997fd666e0a02ece
Daniela Becerra <madajiji@gmail.com>
2016-09-15 16:54:34 -0500

    finish motor string manager header

--- pasos/Project_Headers/HIL/MotorStringManager.h
+++ pasos/Project_Headers/HIL/MotorStringManager.h
@@ -19,4 +19,56 @@
 /*************************************************************************************************/
 #include "types.h"
 
+/*************************************************************************************************/
+/*********************						Defines							**********************/
+/*************************************************************************************************/
+
+#define MOTOR_STRING_PORT				B	/* Specify the digital port where the LED string is connected */
+#define MOTOR_STRING_PIN_START		0   /* Specify the starting pin on the digital port where teh LED String is connected */
+
+/*************************************************************************************************/
+/*********************					Function Prototypes					**********************/
+/*************************************************************************************************/
+
+/*!*-----------------------------------------------------------------------------------------------
+* \name     MotorStringManager_Init
+* \brief    Initalizes the GPIO pins used for the Motor String and it sets it to the initial condition
+*           which is 0x08
+* \param    void
+* \return   void
+-------------------------------------------------------------------------------------------------*/
+void MotorStringManager_Init(void);
+
+/*!*-----------------------------------------------------------------------------------------------
+* \name     MotorStringManager_RotateClockwise
+* \brief    Sets the appropriate sequence for the motor to rotate ClockWise
+* \param    void
+* \return   void
+-------------------------------------------------------------------------------------------------*/
+void MotorStringManager_RotateClockwise(void);
+
+/*!*-----------------------------------------------------------------------------------------------
+* \name     MotorStringManager_RotateCounterClockwise
+* \brief    Sets the appropriate sequence for the motor to rotate Counter ClockWise
+* \param    void
+* \return   void
+-------------------------------------------------------------------------------------------------*/
+void MotorStringManager_RotateCounterClockwise(void);
+
+/*!*-----------------------------------------------------------------------------------------------
+* \name     LedStringManager_RightEndReached
+* \brief    Restarts the sequence from the start when the last index is reached
+* \param    void.
+* \return   void
+-------------------------------------------------------------------------------------------------*/
+bool MotorStringManager_RightEndReached(void);
+
+/*!*-----------------------------------------------------------------------------------------------
+* \name     LedStringManager_RightEndReached
+* \brief    Restarts from the end the sequence when the first index is reached
+* \param    void.
+* \return   void
+-------------------------------------------------------------------------------------------------*/
+bool MotorStringManager_LeftEndReached(void);
+
 #endif /* MOTOR_STRING_H_ */
